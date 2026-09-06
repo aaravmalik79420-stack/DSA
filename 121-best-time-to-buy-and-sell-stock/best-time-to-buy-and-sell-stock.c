9b4c5b40b3fac0215415b0a8f37e86c35d3e3186
aaravmalik79420-stack <aaravmalik79420@gmail.com>
2026-09-06 14:42:52 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (50.35%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.c
@@ -0,0 +1,16 @@
+int maxProfit(int* prices, int pricesSize) {
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for (int i = 1; i < pricesSize; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        }
+
+        if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+
+    return maxProfit;
+}
